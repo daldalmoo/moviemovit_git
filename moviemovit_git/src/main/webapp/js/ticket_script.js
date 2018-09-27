b23730457e5a96b0babed908f7d8c3a8e15f2b4f
Sinhye <dalssak2@naver.com>
2018-09-27 19:44:48 +0900

    경민이랑 취합 전

--- moviemovit_git/src/main/webapp/js/ticket_script.js
+++ moviemovit_git/src/main/webapp/js/ticket_script.js
@@ -30,8 +30,7 @@ $("#movie_area .SelMovieList").click(function(){
   //alert("SelMovieList(mCode) 호출. $(this).val():" + $(this).val());
   if($(this).hasClass("on")==true) {  // 이미 선택된 영화라면 on class 지움
     ResetMovie();
-    //$.post("./cinemaRefresh.do","",mainList);
-  } else {  // 영화를 선택했을 때,
+  } else {  // 영화를 선택했을 때
     $("#movie_area li").removeClass("on");
     $(this).addClass("on");
     MCODE = $(this).val();  // 전역변수 할당
@@ -84,14 +83,53 @@ function cinemaSelect(i) {
   }//if end
 }//cinemaSelect() end
 
+// 주소1 class="click" 일때 주소1 목록 보여주기
+$("#cinema_area .addr1selected").click(function(){
+  $(this).toggleClass("click");
+  $("#cinema_area .addr1list").toggleClass("click");
+});//.addr1selected click() end
+
+// 주소1 목록에서 클릭할때
+$("#cinema_area .addr1list li").click(function(){
+  //alert(".cinema_addr1 .addr1list li html : "+$(this).html());
+  CINEADDR1 = $(this).attr("value");  // 전역변수 설정
+  
+  // 클릭한 주소가 클릭된 주소로 보여짐
+  var msg = $(this).html();
+  $(".addr1selected span").html(msg);
+  
+  // 주소1 목록 접기
+  $(".cinema_addr1 .addr1selected").removeClass("click");
+  $("#cinema_area .addr1list").removeClass("click");
+  
+  // 극장 목록 가져오기
+  if(CINETAB==1) {  // 전체극장
+    cinetab1mainlist();
+  } else if(CINETAB==2) {  // 상영극장
+    cinetab2mainlist();
+  } else if(CINETAB==3) {  // 체인별
+    cinetab3mainlist();
+  }//if end
+  
+});//.addr1list li click() end
+
+// 주소1 변경 -> 영화관 목록 변경
+/*$("#cinema_area .addr1selected span").on('DOMSubtreeModified',function(){  // 변경될때마다 호출
+  //alert("#cinema_area .addr1selected change 됨");
+});//.addr1selected click() end */
+
 function cinetab1mainlist() {  // 전체극장 선택되어있을때 호출
   // display 처리
   $("#cinema_area .nocinema").css('display','none');
   $("#cinema_area .cinema_addr1").css('display','block');
   $("#cinema_area .main_list").css('display','block');
-  if(MCODE!="") {
-    graycinema();  // 상영하는 극장만 검정색 굵게, 그 외 회색 처리
-  }//if end
+  
+  // 주소1에 맞는 극장 목록 가져오기
+  $.post("./cinemaListFromAddr1.do","addr1="+CINEADDR1, mainList);//post end
+  
+  // 영화선택시 상영극장 gray
+  graycinema();
+  
 }//cinetab1mainlist() end
 
 function cinetab2mainlist() {  // 상영극장 선택되어있을때 호출
@@ -103,48 +141,16 @@ function cinetab2mainlist() {  // 상영극장 선택되어있을때 호출
     $("#cinema_area .nocinema").css('display','none');
     $("#cinema_area .cinema_addr1").css('display','block');
     $("#cinema_area .main_list").css('display','block');
-    $.post("./cinemaSelList.do","mCode="+MCODE+"&addr1="+CINEADDR1,mainList);
+    $.post("./cinemaListFromMovieAddr1.do","mCode="+MCODE+"&addr1="+CINEADDR1,mainList);
   }//if end
-}//cinetab1mainlist() end
+}//cinetab2mainlist() end
 
 function cinetab3mainlist() {  // 체인별극장 선택되어있을때 호출
   //display 처리
   $("#cinema_area .nocinema").css('display','none');
   $("#cinema_area .cinema_addr1").css('display','block');
   $("#cinema_area .main_list").css('display','block');
-}//cinetab1mainlist() end
-
-// 주소1 class="click" 일때 주소1 목록 보여주기
-$("#cinema_area .addr1selected").click(function(){
-  $(this).toggleClass("click");
-  $("#cinema_area .addr1list").toggleClass("click");
-});//.addr1selected click() end
-
-// 주소1 목록에서 클릭할때
-$("#cinema_area .addr1list li").click(function(){
-  //alert(".cinema_addr1 .addr1list li html : "+$(this).html());
-  CINEADDR1 = $(this).attr("value");
-  var msg = $(this).html();
-  $(".addr1selected span").html(msg);
-  $(".cinema_addr1 .addr1selected").removeClass("click");
-  $("#cinema_area .addr1list").removeClass("click");
-  addr1change();
-});//.addr1list li click() end
-
-// 주소1 변경 -> 영화관 목록 변경
-function addr1change() {
-  $.post("./addr1change.do","addr1="+CINEADDR1,mainList);  // 극장목록 ajax
-  if(CINETAB==1) {  // 전체극장
-    cinetab1mainlist();
-  } else if(CINETAB==2) {  // 상영극장
-    cinetab2mainlist();
-  } else if(CINETAB==3) {  // 체인별
-    cinetab3mainlist();
-  }//if end
-}//addr1change() end
-/*$("#cinema_area .addr1selected span").on('DOMSubtreeModified',function(){  // 변경될때마다 호출
-  //alert("#cinema_area .addr1selected change 됨");
-});//.addr1selected click() end */
+}//cinetab3mainlist() end
 
 // 극장목록 보여주기
 function mainList(data) {  // Controller에서 msg 값 data로 받아옴
@@ -155,13 +161,19 @@ function mainList(data) {  // Controller에서 msg 값 data로 받아옴
 // 상영극장 굵게 검은색, 그외 회색 처리
 function graycinema() {
   //alert("graycinema 호출");
-  $.post("./graycinemalist.do","mCode="+MCODE,function(cineCodestr) {
-   var cineCode = cineCodestr.split("|");
-   $(".main_list li").css({'font-weight':'normal','color':'#c0c0c0'});  // 모든 극장 회색 처리
-   for(var i=0; i<cineCode.length; i++) {  // 상영하는 극장만 굵게 검은색 처리
-     $(".main_list li[value='"+cineCode[i].trim()+"']").css({'font-weight':'bold','color':'black'});
-   }//for end
-  });//post end
+  //0.01초 시간지연. 이전 코드에서 html이 모두 로딩되길 기다린 후 실행
+  setTimeout(execute, 10); 
+  function execute(){
+     if(MCODE!="") {  // 영화가 선택되어 있으면
+       $.post("./graycinemalist.do","mCode="+MCODE,function(cineCodestr) {
+        var cineCode = cineCodestr.split("|");
+        $(".main_list li").css({'font-weight':'normal','color':'#c0c0c0'});  // 모든 극장 회색 처리
+        for(var i=0; i<cineCode.length; i++) {  // 상영하는 극장만 굵게 검은색 처리
+          $(".main_list li[value='"+cineCode[i].trim()+"']").css({'font-weight':'bold','color':'black'});
+        }//for end
+       });//post end
+     }//if end
+  }//execute() end
 }//graycinema() end
 
 /* ----------- 극장선택 부분 AJAX END ------------ */
