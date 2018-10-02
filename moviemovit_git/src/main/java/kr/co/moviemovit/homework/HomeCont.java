a3b386dfff759a118ecd542494310f58a3bec804
Sinhye <dalssak2@naver.com>
2018-10-02 12:16:37 +0900

    지영 취합 완료

--- moviemovit_git/src/main/java/kr/co/moviemovit/homework/HomeCont.java
+++ moviemovit_git/src/main/java/kr/co/moviemovit/homework/HomeCont.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import kr.co.moviemovit.coupon.CouponDTO;
 import kr.co.moviemovit.movie.MovieDTO;
+import kr.co.moviemovit.ticket.TicketDTO;
 
 
 @Controller
@@ -28,6 +30,20 @@ public class HomeCont {
      mav.setViewName("/homework/homework1");
      ArrayList<MovieDTO> dto = dao.homework1();
      mav.addObject("dto", dto);
+
+     return mav;    
+   }//read() end
+   
+   
+   @RequestMapping(value="/homework/homework2.do", method=RequestMethod.GET)
+   public ModelAndView homework2() {
+     ModelAndView mav = new ModelAndView();
+     mav.setViewName("/homework/homework2");
+     ArrayList<CouponDTO> dto = dao.homework2();
+     ArrayList<TicketDTO> sdto = dao.homework3();
+     
+     mav.addObject("dto", dto);
+     mav.addObject("sdto", sdto);
 
      return mav;    
    }//read() end
